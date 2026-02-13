58f8d277657612252a69989832f2fda5f9d72904
Devin Goble <devin.goble@outlook.com>
2026-02-12 23:48:24 -0800

    add maui blazor app foundation

---
+++ CloudOStat.App/CloudOStat.App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudOStat.App;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ CloudOStat.App/CloudOStat.App/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using CloudOStat.App.Shared.Services;
+using CloudOStat.App.Services;
+
+namespace CloudOStat.App;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        // Add device-specific services used by the CloudOStat.App.Shared project
+        builder.Services.AddSingleton<IFormFactor, FormFactor>();
+
+        builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
